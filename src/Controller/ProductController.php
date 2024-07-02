ef43cc587b113b35c88af3614de422297e8ca197
zied-snoussi <ziedsnoussi.tn@gmail.com>
2024-07-02 20:18:41 +0100

    feat: Add route and controller method for adding a new product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/product/', name: 'app_product')]
     public function index(): Response
     {
         return $this->render('product/index.html.twig', [
@@ -17,9 +18,24 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route("/products", name: "products")]
+    #[Route("/product/products", name: "products")]
     public function fetchProducts(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findAll();
+        return $this->render('products/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route("/product/add", name: "add_product")]
+    public function addProduct(ProductRepository $productRepository): Response
+    {
+        $product = new Product();
+        $product->setName('Product ' . rand(1, 100));
+        $product->setQuantity(rand(1, 100));
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
         $products = $productRepository->findAll();
         return $this->render('products/index.html.twig', [
             'products' => $products,
